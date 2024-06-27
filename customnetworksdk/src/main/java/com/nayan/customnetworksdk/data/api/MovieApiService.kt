eaedb166d0aba51684e9a6a2d107882d820046cd
shahid <shahid@biomentors.online>
2024-06-27 14:57:33 +0530

    first commit

---
+++ customnetworksdk/src/main/java/com/nayan/customnetworksdk/data/api/MovieApiService.kt
@@ -0,0 +1,56 @@
+package com.nayan.customnetworksdk.data.api
+
+import com.nayan.customnetworksdk.data.model.MovieCastCrewResponse
+import com.nayan.customnetworksdk.data.model.MovieDetailsResponse
+import com.nayan.customnetworksdk.data.model.MovieImagesResponse
+import com.nayan.customnetworksdk.data.model.MovieKeywordsResponse
+import com.nayan.customnetworksdk.data.model.MovieRecommendationsResponse
+import com.nayan.customnetworksdk.data.model.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApiService {
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(@Query("api_key") apiKey: String): MovieResponse
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): MovieResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun fetchMovieDetails(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): Response<MovieDetailsResponse>
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun fetchMovieCast(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): Response<MovieCastCrewResponse>
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun fetchMovieMediaVideos(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): MovieResponse
+
+    @GET("movie/{movie_id}/images")
+    suspend fun fetchMovieImages(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): Response<MovieImagesResponse>
+
+    @GET("movie/{movie_id}/recommendations")
+    suspend fun fetchMovieRecommendations(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): MovieRecommendationsResponse
+
+    @GET("movie/{movie_id}/keywords")
+    suspend fun fetchMovieKeywords(
+        @Path("movie_id") movieID: Int,
+        @Query("api_key") apiKey: String
+    ): MovieKeywordsResponse
+}
